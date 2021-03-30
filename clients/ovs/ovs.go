3bbc6207449e907b4a5579f9823ac5ade1393bc2
omer <omerben@campus.technion.ac.il>
2021-03-30 12:09:55 +0300

    adding naive put

--- clients/ovs/ovs.go
+++ clients/ovs/ovs.go
@@ -1,14 +1,17 @@
 package ovs
 
 import (
-//"context"
-"fmt"
-//"strings"
-//"time"
+	//"context"
+	"fmt"
+	"errors"
 
-"github.com/fperf/fperf"
+	//"strings"
+	//"time"
 
-//. "github.com/fperf/etcd/generator"
+	"github.com/ebay/libovsdb"
+	"github.com/fperf/fperf"
+
+	//. "github.com/fperf/etcd/generator"
 
 )
 
@@ -28,7 +31,7 @@ const (
 )
 
 type client struct {
-	//etcd  *clientv3.Client
+	ovs  *libovsdb.OvsdbClient
 	//space *KeySpace
 	op    Op
 }
@@ -51,11 +54,16 @@ func New(fs *fperf.FlagSet) fperf.Client {
 		//space: NewKeySpace(keySize),
 		op:    op,
 	}
-	return &client{}
 }
 
-// Dial to etcd
+// Dial to ovs
 func (c *client) Dial(addr string) error {
+	// TODO: mange configs
+	cli,err := libovsdb.Connect(addr,nil)
+	if err != nil {
+		return err
+	}
+	c.ovs=cli
 	/*
 	endpoints := strings.Split(addr, ",")
 	cli, err := clientv3.New(clientv3.Config{
@@ -71,7 +79,7 @@ func (c *client) Dial(addr string) error {
 	return nil
 }
 
-// Request etcd
+// Request ovs
 func (c *client) Request() error {
 	switch c.op {
 	case Put:
@@ -96,10 +104,79 @@ func doPut(c *client) error {
 	return nil
 }
 func doGet(c *client) error {
+
 	/*
 	_, err := c.etcd.Get(context.Background(), c.space.RandKey())
 	return err
 	*/
+
+	/*
+	SetConfig()
+	if testing.Short() {
+		t.Skip()
+	}
+	*/
+
+	// NamedUUID is used to add multiple related Operations in a single Transact operation
+	namedUUID := "gopher"
+
+	externalIds := make(map[string]string)
+	externalIds["go"] = "awesome"
+	externalIds["docker"] = "made-for-each-other"
+	oMap, err := libovsdb.NewOvsMap(externalIds)
+	// bridge row to insert
+	bridge := make(map[string]interface{})
+	bridge["name"] = libovsdb.bridgeName
+	bridge["external_ids"] = oMap
+
+	// simple insert operation
+	insertOp := libovsdb.Operation{
+		Op:       "insert",
+		Table:    "Bridge",
+		Row:      bridge,
+		UUIDName: namedUUID,
+	}
+
+	// Inserting a Bridge row in Bridge table requires mutating the open_vswitch table.
+	mutateUUID := []libovsdb.UUID{{namedUUID}}
+	mutateSet, _ := libovsdb.NewOvsSet(mutateUUID)
+	mutation := libovsdb.NewMutation("bridges", "insert", mutateSet)
+	// hacked Condition till we get Monitor / Select working
+	condition := libovsdb.NewCondition("_uuid", "!=", libovsdb.UUID{"2f77b348-9768-4866-b761-89d5177ecdab"})
+
+	// simple mutate operation
+	mutateOp := libovsdb.Operation{
+		Op:        "mutate",
+		Table:     "Open_vSwitch",
+		Mutations: []interface{}{mutation},
+		Where:     []interface{}{condition},
+	}
+
+	operations := []libovsdb.Operation{insertOp, mutateOp}
+	reply, err := c.ovs.Transact("Open_vSwitch", operations...)
+
+	if len(reply) < len(operations) {
+		return errors.New("Number of Replies should be atleast equal to number of Operations")
+	}
+	//ok := true
+	for i, o := range reply {
+		if o.Error != "" && i < len(operations) {
+			return errors.New(fmt.Sprintf("Transaction Failed due to an error :", o.Error, " details:", o.Details, " in ", operations[i]))
+			//ok = false
+		} else if o.Error != "" {
+			return errors.New(fmt.Sprintf("Transaction Failed due to an error :", o.Error))
+			//ok = false
+		}
+	}
+	/*
+	if ok {
+		fmt.Println("Bridge Addition Successful : ", reply[0].UUID.GoUUID)
+		bridgeUUID = reply[0].UUID.GoUUID
+	}
+	 */
+
+
+
 	return nil
 }
 func doDelete(c *client) error {
--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/fperf/etcd
 go 1.13
 
 require (
-	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/cenk/hub v1.0.1 // indirect
 	github.com/coreos/bbolt v1.3.3 // indirect
 	github.com/coreos/etcd v3.3.18+incompatible // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd v0.0.0-00010101000000-000000000000 // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/ebay/libovsdb v0.2.0
 	github.com/eclipse/paho.mqtt.golang v1.3.2
 	github.com/fperf/fperf v0.0.0-20190827082251-bba6f2d1209d
 	github.com/fperf/http v0.0.0-20190109124503-5efb2618876a
@@ -19,19 +19,14 @@ require (
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
 	github.com/google/btree v1.0.0 // indirect
-	github.com/google/uuid v1.1.1 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.12.1 // indirect
 	github.com/jonboulle/clockwork v0.1.0 // indirect
 	github.com/json-iterator/go v1.1.8 // indirect
-	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/prometheus/client_golang v1.2.1 // indirect
 	github.com/soheilhy/cmux v0.1.4 // indirect
-	github.com/spf13/cobra v0.0.5 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
-	github.com/urfave/cli v1.22.2 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
 	go.etcd.io/bbolt v1.3.3 // indirect
 	go.etcd.io/etcd v3.3.18+incompatible
@@ -40,7 +35,6 @@ require (
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	golang.org/x/tools v0.0.0-20191029190741-b9c20aec41a5
 	google.golang.org/grpc v1.26.0
-	gopkg.in/cheggaaa/pb.v1 v1.0.28 // indirect
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
 
